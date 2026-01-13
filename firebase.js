7ca35ee4e76af87769195fcea829ae5024d9b051
Rohit23038 <garibwallah770@gmail.com>
2026-01-13 16:18:09 +0530

    Add Firebase config

---
+++ firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAw6mNbrIILP9fNNSzAXuPEUsN7x3T0dsY",
+  authDomain: "schoolerp-cb790.firebaseapp.com",
+  projectId: "schoolerp-cb790",
+  storageBucket: "schoolerp-cb790.firebasestorage.app",
+  messagingSenderId: "1098794553484",
+  appId: "1:1098794553484:web:b8d2abe9b829f8fa7d2ba9"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
